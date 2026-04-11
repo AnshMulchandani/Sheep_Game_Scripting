f81bf3e390ef4c05938c86ae5e351c4f9e65ec31
ansh <ansh.mulchandani01@estudiant.upf.edu>
2026-04-11 20:24:40 +0200

    Shotting Logic Added

--- Assets/Scripts/HayMachine.cs
+++ Assets/Scripts/HayMachine.cs
@@ -4,6 +4,14 @@ public class HayMachine : MonoBehaviour
 {
     public float movementSpeed;
     public float maxMovement = 22.0f;
+    //Reference to the Hay Bale prefab
+    public GameObject hayBalePrefab;
+    //The point from which the hay will to be shot.
+    public Transform haySpawnpoint;
+    //The smallest amount of time between shots
+    public float shootInterval;
+    //A timer that to keep track whether the machine can shoot
+    private float shootTimer; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,16 +22,28 @@ public class HayMachine : MonoBehaviour
     void Update()
     {
         UpdateMovement();
+        UpdateShooting();
         Debug.Log(transform.position.x);
     }
     private void UpdateMovement(){
         float horizontalInput = Input.GetAxisRaw("Horizontal"); // 1
-        if (horizontalInput < 0 && transform.position.x > -maxMovement) // 2
-        {
+        if (horizontalInput < 0 && transform.position.x > -maxMovement){
         transform.Translate(transform.right * - movementSpeed * Time.deltaTime);
         }
         else if (horizontalInput > 0  && transform.position.x < maxMovement){ // 3
         transform.Translate(transform.right * movementSpeed * Time.deltaTime);
         }
+    }
+    private void ShootHay(){
+        //Creates instances of the prefab
+        Instantiate(hayBalePrefab, haySpawnpoint.position,
+        Quaternion.identity);
+    }
+    private void UpdateShooting() {
+        shootTimer -= Time.deltaTime;
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space)) {
+            shootTimer = shootInterval;
+            ShootHay();
         }
+    }
 }
